41d6db461900b47192ccbd4ac5d34f225febde9f
NutsaTati <21100278@ibsu.edu.ge>
2022-06-24 00:08:19 +0400

    Add project files.

---
+++ TicTacToe/Game.h
@@ -0,0 +1,125 @@
+#pragma once
+#include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "Game.h"
+using namespace std;
+using namespace sf;
+
+char game[9] = { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
+int game_x[9] = { 27,227,410,27,227,410,27,227,410 };
+int game_y[9] = { 40,40,40,220,220,220,420,420,420 };
+
+class GamePlay
+{
+
+private:
+
+	char t = 'N';
+	int check_o() {
+
+		char a = game[0];
+
+		for (int i = 1; i < 9; i++) {
+
+			if (a == game[i] && (i + 1) != 3 && (i + 1) != 6 && isdigit(game[i + 1])) { return (i + 1); }
+
+			else a = game[i];
+
+		}
+
+
+
+		for (int i = 0; i < 9; i++) {
+
+			if (game[i] == game[i + 3]) {
+
+				if (isdigit(game[i + 6])) return i + 6;
+
+				else if (isdigit(game[i - 3])) return i - 3;
+
+			}
+
+		}
+
+
+
+		return 11;
+	}
+	void o_turn() {
+		int r = 1 + (rand() % 9);
+
+
+		bool tie = true;
+		for (int i = 0; i < 9; i++) {
+			if (isdigit(game[i])) tie = false;
+		}
+
+		if (tie == true) {
+			t = 't'; return;
+		}
+		if (check_o() != 11)r = check_o();
+		else {
+			while (!isdigit(game[r]))r = 1 + (rand() % 9);
+		}
+		game[r] = 'O';
+
+
+
+	}
+
+
+
+
+
+public:
+
+	char check_for_winner() {
+
+		if (game[0] == game[1] && game[0] == game[2]) return game[2];
+		if (game[3] == game[4] && game[3] == game[5]) return game[5];
+		if (game[6] == game[7] && game[6] == game[8]) return game[8];
+		if (game[0] == game[3] && game[0] == game[6]) return game[6];
+		if (game[1] == game[4] && game[4] == game[7]) return game[7];
+		if (game[2] == game[5] && game[5] == game[8]) return game[8];
+		if (game[0] == game[4] && game[4] == game[8]) return game[8];
+		if (game[2] == game[4] && game[4] == game[6]) return game[6];
+		return t;
+
+
+	}
+	void input(int x, int y) {
+
+
+		if (x < 205) {
+			if (y < 200 && isdigit(game[0])) { game[0] = 'X'; o_turn(); }
+			else if (y > 200 && y < 395 && isdigit(game[3])) { game[3] = 'X'; o_turn(); }
+			else if (y > 395 && isdigit(game[6])) { game[6] = 'X'; o_turn(); }
+		}
+		else if (x > 205 && x < 395) {
+			if (y < 200 && isdigit(game[1])) { game[1] = 'X'; o_turn(); }
+			else if (y > 200 && y < 395 && isdigit(game[4])) { game[4] = 'X'; o_turn(); }
+			else if (y > 395 && isdigit(game[7])) { game[7] = 'X'; o_turn(); }
+
+		}
+		else if (x > 395) {
+			if (y < 200 && isdigit(game[2])) { game[2] = 'X'; o_turn(); }
+			else if (y > 200 && y < 395 && isdigit(game[5])) { game[5] = 'X'; o_turn(); }
+			else if (y > 395 && isdigit(game[8])) { game[8] = 'X'; o_turn(); }
+
+		}
+	
+	}
+	void reset_game()
+	{
+		t = 'N';
+		winner = 'N';
+		for (int i = 0; i < 9; i++) {
+			game[i] = i + '0';
+		}
+	}
+	char winner = check_for_winner();
+
+};
+
---
+++ TicTacToe/Source.cpp
@@ -0,0 +1,141 @@
+#include "Game.h"
+RenderWindow win(VideoMode(600, 600), "Tic Tac Toe", sf::Style::Close | sf::Style::Titlebar);
+
+bool endscreen = false;
+
+int main(){
+
+
+	// LOAD IMAGES
+
+	Texture background;
+	Texture x_texture;
+	Texture o_texture;
+	Texture won; Texture lost; Texture tie;
+
+	if (!x_texture.loadFromFile("assets/x.png"))cout << "X not found" << endl;
+	Sprite X;
+	if (!background.loadFromFile("assets/background.png"))cout << "B not found" << endl;
+	Sprite Background;
+	if (!o_texture.loadFromFile("assets/O.png"))cout << "O not found" << endl;
+	Sprite O;
+	if (!won.loadFromFile("assets/YOU_WON.png"))cout << "YOU_WON not found" << endl;
+	Sprite Won;
+	if (!lost.loadFromFile("assets/YOU_LOST.png"))cout << "YOU_LOST not found" << endl;
+	Sprite Lost;
+	if (!tie.loadFromFile("assets/TIE.png"))cout << "TIE not found" << endl;
+	Sprite Tie;
+
+	Background.setTexture(background);
+	X.setTexture(x_texture);
+	O.setTexture(o_texture);
+	Lost.setTexture(lost); 	Won.setTexture(won); Tie.setTexture(tie);
+
+
+
+	X.scale(Vector2f(0.25, 0.25));
+	O.scale(Vector2f(0.25, 0.25));
+
+	// END LOADING IMAGES
+
+	GamePlay gameplay;
+	int turn = 0;
+	endscreen = false;
+
+
+	while (win.isOpen()) {
+
+		char winner = gameplay.check_for_winner();
+
+		Event e;
+
+
+		
+
+		while (win.pollEvent(e)) {
+			Vector2i mouse_pos = Mouse::getPosition(win);
+			int x = mouse_pos.x, y = mouse_pos.y;
+			cout << mouse_pos.x << "    " << mouse_pos.y << endl;
+			switch (e.type) {
+			case Event::Closed:
+				win.close();
+				break;
+			case Event::KeyPressed:
+				if(e.key.code == Keyboard::Escape)
+					win.close();
+					break;
+	
+			case Event::MouseButtonPressed:
+				if (endscreen) {
+
+					if (x > 152 && x < 456 && y >297 && y < 376) {
+						gameplay.reset_game();
+
+						main();
+						
+					}
+					else if (x > 152 && x < 456 && y>393 && y < 470) {
+						win.close();
+					}
+				}
+				cout << "Mouse button pressed" << endl;
+				if (winner == 'N' && !endscreen) { 
+					Vector2i mouse_pos = Mouse::getPosition(win);
+					gameplay.input(mouse_pos.x, mouse_pos.y);break;
+				}
+			}
+	}
+
+
+		if (winner != 'N') {
+			win.clear();
+			win.draw(Background);
+			if (turn == 0) {
+				for (int i = 0; i < 9; i++) {
+					if (game[i] == 'X') {
+						X.setPosition(Vector2f(game_x[i], game_y[i]));
+						win.draw(X);
+						turn++;
+
+
+					}if (game[i] == 'O') {
+						O.setPosition(Vector2f(game_x[i], game_y[i]));
+						win.draw(O);
+						turn++;
+
+
+					}
+				}
+				win.display();
+				endscreen = true;
+				sleep(sf::seconds(0.5));
+
+			}
+			else {
+
+				if (winner == 'X')win.draw(Won);
+				else if (winner == 'O')win.draw(Lost);
+				else win.draw(Tie);
+				win.display();
+			}
+
+		}
+		else {
+			// DISPLAYING GAME
+
+			win.clear();
+			win.draw(Background);
+
+			for (int i = 0; i < 9; i++) {
+				if (game[i] == 'X') {
+					X.setPosition(Vector2f(game_x[i], game_y[i]));
+					win.draw(X);
+				}if (game[i] == 'O') {
+					O.setPosition(Vector2f(game_x[i], game_y[i]));
+					win.draw(O);
+				}
+			}
+			win.display();
+		}
+	}
+}
